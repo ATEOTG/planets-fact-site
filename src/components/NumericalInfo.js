971350e9df428cc0e186ad42d3ba7cc7f71fbc1e
ATEOTG <bryansantos9797@gmail.com>
2022-08-18 15:41:51 -0400

    numerical information component and styling

--- src/components/NumericalInfo.js
+++ src/components/NumericalInfo.js
@@ -2,7 +2,7 @@ import classes from "./NumericalInfo.module.css";
 
 function NumericalInfo(props) {
   return (
-    <div className={`${classes["info-cont"]}`}>
+    <div className={`${classes["info-cont"]} ${"centered"}`}>
       <div className={classes.info}>
         <p className={classes["info-desc"]}>ROTATION TIME</p>
         <p className={classes["info-number"]}>{props.rotation}</p>
--- src/pages/Earth.js
+++ src/pages/Earth.js
@@ -1,5 +1,6 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import InnerNav from "../components/InnerNav";
+import NumericalInfo from "../components/NumericalInfo";
 import classes from "./Earth.module.css";
 
 function Earth() {
@@ -12,6 +13,13 @@ function Earth() {
       <Routes>
         <Route path="earth" to={<Navigate replace to="overview" />} />
       </Routes>
+
+      <NumericalInfo
+        rotation={"0.99 DAYS"}
+        revolution={"365.26 DAYS"}
+        radius={"6,371 KM"}
+        temp={"16°C"}
+      />
     </div>
   );
 }
