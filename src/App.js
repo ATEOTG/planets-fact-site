fea60f502941f9e0bf8948e654e44a7d3f0d09a4
ATEOTG <bryansantos9797@gmail.com>
2022-08-18 18:57:41 -0400

    added Mercury page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Earth from "./pages/Earth";
+import Mercury from "./pages/Mercury";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate replace to="/earth" />} />
         <Route path="/earth/*" element={<Earth />} />
+        <Route path="/mercury/*" element={<Mercury />} />
       </Routes>
     </Fragment>
   );
