e16ef27b968188090a26826d13b9d3a45767c8cf
ATEOTG <bryansantos9797@gmail.com>
2022-08-18 19:05:00 -0400

    added Venus page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Earth from "./pages/Earth";
 import Mercury from "./pages/Mercury";
+import Venus from "./pages/Venus";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Navigate replace to="/earth" />} />
         <Route path="/earth/*" element={<Earth />} />
         <Route path="/mercury/*" element={<Mercury />} />
+        <Route path="/venus/*" element={<Venus />} />
       </Routes>
     </Fragment>
   );
