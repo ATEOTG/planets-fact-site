aa53d9a26c0937eff61d431605845566f6c19579
ATEOTG <bryansantos9797@gmail.com>
2022-08-19 15:24:27 -0400

    refactored dropdown menu code

--- src/components/MobileDropdown.js
+++ src/components/MobileDropdown.js
@@ -1,164 +1,75 @@
 import { Link } from "react-router-dom";
 import IconChevron from "./svg/IconChevron";
 import classes from "./MobileDropdown.module.css";
+import MobileDropdownItem from "./MobileDropdownItem";
 
 function MobileDropdown(props) {
   return (
     <nav className={classes["dropdown-menu"]}>
       <ul className={classes["dropdown-list"]}>
         <li>
-          <Link
-            to="/mercury/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#DEF4FC" }}
-                ></div>
-                <p className={classes["list-link-text"]}>MERCURY</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>
+            path={"/mercury/overview"}
+            circleColor={"#DEF4FC"}
+            planetName={"MERCURY"}
+          />
         </li>
         <li>
-          {" "}
-          <Link
-            to="/venus/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            {" "}
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#F7CC7F" }}
-                ></div>
-                <p className={classes["list-link-text"]}>VENUS</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>
+            path={"/venus/overview"}
+            circleColor={"#F7CC7F"}
+            planetName={"VENUS"}
+          />
         </li>
         <li>
-          {" "}
-          <Link
-            to="/earth/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            {" "}
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#545BFE" }}
-                ></div>
-                <p className={classes["list-link-text"]}>EARTH</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>
+            path={"/earth/overview"}
+            circleColor={"#545BFE"}
+            planetName={"EARTH"}
+          />
         </li>
         <li>
-          <Link
-            to="/mars/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            {" "}
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#FF6A45" }}
-                ></div>
-                <p className={classes["list-link-text"]}>MARS</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>{" "}
+            path={"/mars/overview"}
+            circleColor={"#FF6A45"}
+            planetName={"MARS"}
+          />
         </li>
         <li>
-          <Link
-            to="/jupiter/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            {" "}
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#ECAD7A" }}
-                ></div>
-                <p className={classes["list-link-text"]}>JUPITER</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>{" "}
+            path={"/jupiter/overview"}
+            circleColor={"#ECAD7A"}
+            planetName={"JUPITER"}
+          />
         </li>
         <li>
-          {" "}
-          <Link
-            to="/saturn/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            {" "}
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#FCCB6B" }}
-                ></div>
-                <p className={classes["list-link-text"]}>SATURN</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>
+            path={"/saturn/overview"}
+            circleColor={"#FCCB6B"}
+            planetName={"SATURN"}
+          />
         </li>
         <li>
-          <Link
-            to="/uranus/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            {" "}
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#65F0D5" }}
-                ></div>
-                <p className={classes["list-link-text"]}>URANUS</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>{" "}
+            path={"/uranus/overview"}
+            circleColor={"#65F0D5"}
+            planetName={"URANUS"}
+          />
         </li>
         <li>
-          <Link
-            to="/neptune/overview"
-            className={classes.link}
+          <MobileDropdownItem
             onClick={props.onMenuClick}
-          >
-            {" "}
-            <div className={classes["list-link"]}>
-              <div>
-                <div
-                  className={classes.circle}
-                  style={{ "background-color": "#497EFA" }}
-                ></div>
-                <p className={classes["list-link-text"]}>NEPTUNE</p>
-              </div>
-              <IconChevron />
-            </div>
-          </Link>{" "}
+            path={"/neptune/overview"}
+            circleColor={"#497EFA"}
+            planetName={"NEPTUNE"}
+          />
         </li>
       </ul>
     </nav>
---
+++ src/components/MobileDropdownItem.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import IconChevron from "./svg/IconChevron";
+import classes from "./MobileDropdownItem.module.css";
+
+function MobileDropdownItem(props) {
+  return (
+    <Link to={props.path} className={classes.link} onClick={props.onClick}>
+      <div className={classes["list-link"]}>
+        <div>
+          <div
+            className={classes.circle}
+            style={{ "background-color": `${props.circleColor}` }}
+          ></div>
+          <p className={classes["list-link-text"]}>{props.planetName}</p>
+        </div>
+        <IconChevron />
+      </div>
+    </Link>
+  );
+}
+
+export default MobileDropdownItem;
