71db3391df3cee315558f4256ac7f38a5e9f046a
ATEOTG <bryansantos9797@gmail.com>
2022-08-20 13:55:51 -0400

    added styling (border on the top) to outer link for desktop version

--- src/components/OuterNav.js
+++ src/components/OuterNav.js
@@ -4,14 +4,39 @@ import classes from "./OuterNav.module.css";
 function OuterNav() {
   return (
     <div className={classes["outernav-cont"]}>
-      <Link to="/mercury/overview"> MERCURY</Link>
-      <Link to="/venus/overview"> VENUS</Link>
-      <Link to="/earth/overview"> EARTH</Link>
-      <Link to="/mars/overview"> MARS</Link>
-      <Link to="/jupiter/overview"> JUPITER</Link>
-      <Link to="/saturn/overview"> SATURN</Link>
-      <Link to="/uranus/overview"> URANUS</Link>
-      <Link to="/neptune/overview"> NEPTUNE</Link>
+      <Link to="/mercury/overview" id={classes.mercury}>
+        {" "}
+        <div></div>
+        MERCURY
+      </Link>
+      <Link to="/venus/overview" id={classes.venus}>
+        <div></div>
+        VENUS
+      </Link>
+      <Link to="/earth/overview" id={classes.earth}>
+        <div></div>
+        EARTH
+      </Link>
+      <Link to="/mars/overview" id={classes.mars}>
+        <div></div>
+        MARS
+      </Link>
+      <Link to="/jupiter/overview" id={classes.jupiter}>
+        <div></div>
+        JUPITER
+      </Link>
+      <Link to="/saturn/overview" id={classes.saturn}>
+        <div></div>
+        SATURN
+      </Link>
+      <Link to="/uranus/overview" id={classes.uranus}>
+        <div></div>
+        URANUS
+      </Link>
+      <Link to="/neptune/overview" id={classes.neptune}>
+        <div></div>
+        NEPTUNE
+      </Link>
     </div>
   );
 }
